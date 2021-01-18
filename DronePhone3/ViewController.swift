506910a2c35cbac7ccfcee7cd49adbfab8f15ccd
Thomas Deng <thomasxdeng@gmail.com>
2021-01-17 21:36:18 -0800

    Added a WebView

--- DronePhone3/ViewController.swift
+++ DronePhone3/ViewController.swift
@@ -4,16 +4,40 @@
 //
 //  Created by DENNIS DIXON on 1/18/21.
 //
-
+//
+//  ViewController.swift
+//  DronePhone
+//
+//  Created by Thomas Deng on 1/17/21.
+//
+import WebKit
 import UIKit
-
-class ViewController: UIViewController {
-
+import Foundation
+class ViewController: UIViewController, WKNavigationDelegate{
+    var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+         
+         // 1
+         let url = URL(string: "https://www.ioscreator.com/tutorials/webview-ios-tutorial")!
+         webView.load(URLRequest(url: url))
+           
+         // 2
+         let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+         toolbarItems = [refresh]
+         navigationController?.isToolbarHidden = false
+        
     }
-
-
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+        }
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    
 }
-
